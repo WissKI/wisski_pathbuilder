271a08774e85bcf3741e76da1ff6c79f7304f7a6
WissKI <m.fichtner@wiss-ki.eu>
2013-02-28 14:44:19 +0100

    Fixed some further bugs, added correct javascript behaviour

--- wisski_pathbuilder.js
+++ wisski_pathbuilder.js
@@ -6,15 +6,18 @@ Drupal.behaviors.wisski_override_autocomplete = function () {
       
       // Select item if the right key or mousebutton was pressed
       if (this.selected && ((keycode && keycode != 46 && keycode != 8 && keycode != 27) || !keycode)) {
-                
+                        
         var id = this.input.id;
         id = id.replace(/^(edit-[-\d]+)(-\d+)$/, "#$1-auth-data$2");
         var hiddenfield = $(id);
         hiddenfield = hiddenfield[0];
 
-        if (/^\[vid:(\d+)\] \[ind:([^\]]+)\] (.*)$/.exec(this.selected.autocompleteValue)) {
-          this.input.value = RegExp.$3;
+//        if (/^\[vid:(\d+)\] \[ind:([^\]]+)\] (.*)$/.exec(this.selected.autocompleteValue)) {
+//          this.input.value = RegExp.$3;
+        if (/^\[vid:(\d+)\] \[ind:([^\]]+)\] \[x:([^\]]+)\] (.*)$/.exec(this.selected.autocompleteValue)) {
+          this.input.value = RegExp.$4;
           hiddenfield.value = this.selected.autocompleteValue;
+          
 
         } else {
           this.input.value = this.selected.autocompleteValue;
@@ -42,6 +45,7 @@ Drupal.behaviors.wisski_override_autocomplete = function () {
         //          geocoder.process(this.input.value);
         //        }
       }
+
       this.selected = false;      
       return true;
     };
@@ -58,14 +62,25 @@ Drupal.behaviors.correct_autocomplete_select = function () {
   }
 }
 */
+
+
 $(document).ready(function(){
-
-
-
+  $('form').bind("keydown", function(e) {
+    var code = e.keyCode || e.which; 
+    if (code  == 13) {
+//          alert("don't!");               
+       e.preventDefault();
+       return false;
+    }
+  });
+  
   $('.form-item').keydown(function(event) {
     if (event.which == 13) {
+//      alert("stop!");
+      event.preventDefault();
       event.stopPropagation();
       event.preventDefault();
+//      alert("stopped!");
       return false;
 //      alert("haha!");
 //      event.preventDefault();
@@ -79,18 +94,6 @@ $(document).ready(function(){
 //  if ($("#autocomplete").length == 0) {
     }
   });
-
-/*  $('.wki-button-dupl').unbind('keypress');
-  $('.wki-button-dupl').unbind('keydown');
-  $('.wki-button-dupl').bind('keypress', (function(event){
-      alert("haha");
-
-    if (event.keyCode == 10 || event.keyCode == 13) { 
-      alert("haha");
-      return false;
-    }
-  }));*/
-  
   
 });
 
